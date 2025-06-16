1c5dc25f92ed96d715671418a4bd5b5550516494
eunjun <dmswns3220@daum.net>
2025-06-16 19:55:44 +0900

    ddd

--- src/main/java/com/example/demo/domain/model/User.java
+++ src/main/java/com/example/demo/domain/model/User.java
@@ -1,6 +1,7 @@
 package com.example.demo.domain.model;
 
 
+import com.example.demo.domain.model.enums.Role;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -18,9 +19,15 @@ public class User {
 
     private String name;
 
+    private int admin;
+
     private boolean deleteFlag;
 
     private LocalDateTime createDate;
 
     private LocalDateTime updateDate;
+
+    public String getRole() {
+        return this.admin == 1 ? String.valueOf(Role.ADMIN) : String.valueOf(Role.USER);
+    }
 }
--- src/main/java/com/example/demo/domain/model/dto/RegistResponseDto.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.domain.model.dto;
-
-import com.example.demo.global.config.CommonDto;
-
-public class RegistResponseDto {
-
-
-}
